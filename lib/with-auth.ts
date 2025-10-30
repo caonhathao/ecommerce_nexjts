d147bf2be7b8b8b92924d205ba6489ed14a99995
Le Vu Hoang <bx1536hoang@gmail.com>
2025-10-30 22:29:23 +0700

    fix withAuth

--- lib/with-auth.ts
+++ lib/with-auth.ts
@@ -1,20 +1,22 @@
-import {auth} from '@/lib/auth';
-import {headers} from 'next/headers';
-import {NextRequest, NextResponse} from 'next/server';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
 
 export function withAuth(
-    handler: (userId: string, request: NextRequest) => Promise<NextResponse>
+  handler: (userId: string, request: NextRequest) => Promise<NextResponse>
 ) {
-    return async function (request: NextRequest) {
-        // const session = await getServerSession(authOptions);
-        // const userId = session?.user?.id;
-        const userId = "5a5ed0a1-66db-41ce-a2c5-dece655d8166"; // ✅ Dùng tạm để test
+  return async function (request: NextRequest) {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+    const userId = session?.user?.id;
+    // const userId = "5a5ed0a1-66db-41ce-a2c5-dece655d8166"; // ✅ Dùng tạm để test
 
-        if (!userId) {
-            return NextResponse.json({error: 'Unauthorized'}, {status: 401});
-        }
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
 
-        // ✅ Gọi handler với userId và request
-        return handler(userId, request);
-    };
+    // ✅ Gọi handler với userId và request
+    return handler(userId, request);
+  };
 }
