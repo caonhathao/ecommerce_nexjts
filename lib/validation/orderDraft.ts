fb54ec3ae16b4ea0765e6395c5e2184554d4544b
Le Vu Hoang <bx1536hoang@gmail.com>
2025-11-18 13:29:18 +0700

    untracked file lib/prisma

---
+++ lib/validation/orderDraft.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const createOrderDraftSchema = z.object({
+  notes: z.string().optional(),
+  items: z.array(
+    z.object({
+      productId: z.uuid(),
+      variantId: z.uuid().optional(),
+      quantity: z.number().min(1),
+    })
+  ),
+  voucher: z.array(z.object({ code: z.string() })),
+
+  currency: z.enum(['VND', 'USD']).optional(),
+});
---
+++ lib/with-auth.ts
@@ -0,0 +1,20 @@
+import { auth, getSessionUser } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export function withAuth(
+  handler: (userId: string, request: NextRequest) => Promise<NextResponse>
+) {
+  return async function (request: NextRequest) {
+    const session = await getSessionUser()
+    const userId = session?.user?.id;
+    // const userId = "5a5ed0a1-66db-41ce-a2c5-dece655d8166"; // ✅ Dùng tạm để test
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    // ✅ Gọi handler với userId và request
+    return handler(userId, request);
+  };
+}
