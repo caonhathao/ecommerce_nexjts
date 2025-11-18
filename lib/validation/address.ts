5d2e37f99f1c24d5897a8cbb0279a407e6521151
Le Vu Hoang <bx1536hoang@gmail.com>
2025-11-18 13:24:23 +0700

    Stop tracking lib folder

--- lib/validation/address.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const createAddressSchema = z.object({
-  line1: z.string(),
-  ward: z.string(),
-  district: z.string(),
-  city: z.string(),
-  country: z.string(),
-});
