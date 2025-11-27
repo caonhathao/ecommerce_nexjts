a856d7526a07a2145ed95a5b0484fd9ad7646676
caonhathao <caonhathao2372004@gmail.com>
2025-11-27 12:40:05 +0700

    hotfix: fix wrong call api

--- app/manager/categories/_funcs/funcs.tsx
+++ app/manager/categories/_funcs/funcs.tsx
@@ -13,7 +13,9 @@ export const handleDelete = async ({
   t: (key: string) => string;
 }) => {
   try {
-    const response = await deleteData({ url: paths.cateogry.del_one(id) });
+    const response = await deleteData({
+      url: paths.manager.category.del_one(id),
+    });
     if (response.ok) {
       toast(t('t_action_noti'), {
         description: t('t_del_desc_noti'),
